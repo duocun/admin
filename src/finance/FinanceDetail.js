2e3f8d6cf3c991f6cdeec62ff09dbff8253d3458
zlkca <likzhang@gmail.com>
2020-05-31 13:23:46 -0400

    add transaction

--- src/finance/FinanceDetail.js
+++ src/finance/FinanceDetail.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, {useEffect} from "react";
 import AccountSearchBox from "../account/AccountSearchBox";
 import AccountList from "../account/AccountList";
 import TransactionList from "./TransactionList";
 import FinanceHeader from "./FinanceHeader";
 import { NavBar, Menu } from "../ui/NavBar";
+import {TransactionAPI} from "../transaction/API";
 
 import "./FinanceDetail.scss";
 
 //only for users
 const FinanceDetail = () => {
+
+  useEffect(() => {
+    const trSvc = new TransactionAPI();
+    trSvc.find({fromId:'5cae0a7d9687ac4a075e2f56'}).then(r => {
+      const k = r;
+    });
+  }, []);
+
   return (
     <div className="page">
       <div className="nav-menu-bar">
@@ -17,8 +26,11 @@ const FinanceDetail = () => {
       <div className="page-content finance-page">
         <FinanceHeader />
         <div className="finance-detail">
-          <AccountSearchBox />
-          <AccountList />
+          {/* <AccountSearchBox />
+          <AccountList /> */}
+
+
+
           <TransactionList />
         </div>
       </div>
@@ -27,3 +39,4 @@ const FinanceDetail = () => {
 };
 
 export default FinanceDetail;
+
--- src/transaction/API.js
+++ src/transaction/API.js
@@ -1,21 +1,12 @@
 import { Http, HttpStatus } from '../API';
+import queryString from "query-string";
 
 export class TransactionAPI {
   url = 'Transactions';
   http = new Http();
 
-
-  find(query = null) {
-    return new Promise((resolve, reject) => {
-      this.http.get(this.url + '/', query).then(rsp => {
-        if (rsp.status === HttpStatus.OK.code) {
-          resolve(rsp.data);
-        } else {
-          reject();
-        }
-      }).catch(err=>{
-        reject(err);
-      })
-    });
+  async find(params = null) {
+    const q = queryString.stringify(params);
+    return await this.http.get(this.url + '/?' + q);
   }
 }
